7dfc9db678b418c7a1206681545fb84f60d98722
GHC <ssjgssgokuvegeta@gmail.com>
2023-01-28 16:24:20 -0500

    Lab 1

---
+++ tekkenreborn/src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
@@ -0,0 +1,17 @@
+package com.cpan252.tekkenreborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cpan252.tekkenreborn.controller.HomeController;
+
+@SpringBootApplication
+public class TekkenrebornApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(TekkenrebornApplication.class, args);
+		HomeController controller = context.getBean(HomeController.class);
+	}
+
+}
