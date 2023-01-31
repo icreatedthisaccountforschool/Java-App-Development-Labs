9189dc8527522b80da750bad2857bfd159abc72f
GHC <ssjgssgokuvegeta@gmail.com>
2023-01-31 16:13:00 -0500

    Lab 2

--- tekkenreborn/src/main/java/com/cpan252/tekkenreborn/controller/DesignController.java
+++ tekkenreborn/src/main/java/com/cpan252/tekkenreborn/controller/DesignController.java
@@ -1,22 +1,28 @@
 package com.cpan252.tekkenreborn.controller;
 
 import java.util.EnumSet;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.cpan252.tekkenreborn.model.Fighter;
+import com.cpan252.tekkenreborn.model.FighterPool;
 import com.cpan252.tekkenreborn.model.Fighter.Anime;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 @RequestMapping("/design")
+@SessionAttributes("fighterPool")
 public class DesignController {
     @GetMapping
     public String design() {
@@ -25,10 +31,15 @@ public class DesignController {
 
     @ModelAttribute
     public void animes(Model model) {
-        var animes = EnumSet.allOf(Anime.class).stream()
-                .map(Anime::getTitle).collect(Collectors.toList());
+        var animes = EnumSet.allOf(Anime.class);
+                //.map(Anime::getTitle).collect(Collectors.toList());
         model.addAttribute("animes", animes);
         log.info("animes converted to string:  {}", animes);
+    }
+
+    @ModelAttribute(name = "fighterPool")
+    public FighterPool fighterPool() {
+        return new FighterPool();
     }
 
     @ModelAttribute
@@ -36,5 +47,15 @@ public class DesignController {
         return Fighter
                 .builder()
                 .build();
+    }
+
+    @PostMapping
+    public String processFighterAddition(@Valid Fighter fighter,
+            @ModelAttribute FighterPool pool, Errors errors) {
+        if (errors.hasErrors()){
+            return "design";
+        }
+        pool.add(fighter);
+        return "redirect:/design";
     }
 }
