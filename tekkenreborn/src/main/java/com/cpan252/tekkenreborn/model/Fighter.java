55593b88e4bd619c7bc0c1f25ff88e0f0da42745
GHC <ssjgssgokuvegeta@gmail.com>
2023-02-28 17:05:02 -0500

    Lab 5

--- tekkenreborn/src/main/java/com/cpan252/tekkenreborn/model/Fighter.java
+++ tekkenreborn/src/main/java/com/cpan252/tekkenreborn/model/Fighter.java
@@ -3,21 +3,44 @@ package com.cpan252.tekkenreborn.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@Table
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
 public class Fighter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @Max(100)
+    private int damagePerHit;
+    @Min(1000)
+    private int health;
+    @DecimalMin(value = "0.1", inclusive = true)
+    @DecimalMax(value = "10.0", inclusive = true)
+    private BigDecimal resistance;
+    private Anime animeFrom;
+
+    @Builder.Default
+    private LocalDate createdAt = LocalDate.now();
+
     public enum Anime {
         NARUTO("Naruto"), BLEACH("Bleach"), ONE_PIECE("One Piece"), TEKKEN("Tekken");
 
@@ -31,19 +54,4 @@ public class Fighter {
             return title;
         }
     }
-
-    @Id
-    private long id;
-    @NotBlank
-    private String name;
-    @Max(100)
-    private int damagePerHit;
-    @Min(1000)
-    private int health;
-    @DecimalMin(value = "0.1", inclusive = true)
-    @DecimalMax(value = "10.0", inclusive = true)
-    private BigDecimal resistance;
-    private Anime animeFrom;
-    @Builder.Default
-    private LocalDateTime createdAt = LocalDateTime.now();
 }
