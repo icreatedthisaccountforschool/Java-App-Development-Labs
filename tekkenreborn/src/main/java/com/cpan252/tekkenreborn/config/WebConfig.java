f0856c4d46255559b3272bef7c4e748d9661f55f
GHC <ssjgssgokuvegeta@gmail.com>
2023-04-07 17:16:02 -0400

    Lab 9

--- tekkenreborn/src/main/java/com/cpan252/tekkenreborn/config/WebConfig.java
+++ tekkenreborn/src/main/java/com/cpan252/tekkenreborn/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.cpan252.tekkenreborn.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login");
         registry.addViewController("/").setViewName("design.html");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
